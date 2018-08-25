e77ea4700616b4e2120633e6d5ae95555c157bde
Goran Mihajlov <goran.mihajlov@gmail.com>
2018-08-26 01:15:12 +0200

    CORS Solved

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,15 +12,10 @@
 */
 
 Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
+    'middleware' => 'api'
 ], function ($router) {
-
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
-
 });
